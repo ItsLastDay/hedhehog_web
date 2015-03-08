227cdee51b0fd961c71d77eed35fd8c44f79ff6a
Mikhail Koltsov <misha@koltsov.su>
2015-03-08 23:09:51 +0300

    Tweaked admin page to show short descriptions on the list.
    Slightly reworked models in admin page representation (__unicode__ => __str__,
     changed __str__ to better explain the entity).
    
    Added 'Course' model to catch the sequence of lessons.

--- hedgehog/summer/admin.py
+++ hedgehog/summer/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from summer.models import User, Lesson, Post
+from summer.models import User, Lesson, Post, Course
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'included_in_course', \
+            'number_in_course']
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(User)
+admin.site.register(Course)
--- hedgehog/summer/models.py
+++ hedgehog/summer/models.py
@@ -9,14 +9,27 @@ class Lesson(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField()
 
-    def __unicode__(self):
-        return 'Lesson %s on %s' % (self.title, self.youtube_url)
+    included_in_course = models.ForeignKey('Course', blank=True, related_name='lessons')
+    number_in_course = models.IntegerField(db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return 'lessons/%d' % self.id
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     pub_date = models.DateTimeField(db_index=True)
     description = models.TextField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.description
 
     class Meta:
@@ -40,7 +53,7 @@ class User(auth.models.User):
     def set_password(self, psw):
         super(auth.models.User, self).set_password(psw)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
 
     class Meta:
