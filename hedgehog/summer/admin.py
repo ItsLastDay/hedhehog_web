b186ea20a1826598fa0ae9f877f056627b28ab71
Mikhail Koltsov <misha@koltsov.su>
2015-03-08 22:00:58 +0300

    Switched to bootstrap 3.x. It seem more promising and convenient. Trying to make header. Going towards video pages.

--- hedgehog/summer/admin.py
+++ hedgehog/summer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from summer.models import User, Lesson, Post
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Lesson)
+admin.site.register(User)
--- hedgehog/summer/views.py
+++ hedgehog/summer/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_GET, require_POST
 
 # Create your views here.
+
+@require_GET
+def main_page(request):
+    return render(request, 'base.html', dict())
